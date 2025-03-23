484be44e0e1812dd8d7e9b74ba63b847ead0005c
mitblack1412h2 <73018988+mitblack1412h2@users.noreply.github.com>
2025-03-23 13:27:00 +0700

    Add files via upload

--- database/store.sql
+++ database/store.sql
@@ -1,1 +1,156 @@
 
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+
+--
+-- Database: `store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `price`) VALUES
+(1, 'Iphone11', 17000),
+(2, 'Vivo', 15000),
+(3, 'Samsum', 16000),
+(4, 'Xiaomi', 13000),
+(5, 'Titan Model #light', 13000),
+(6, 'Titan Model', 3000),
+(7, 'HMT Milan', 8000),
+(8, 'Favre Lueba ', 18000),
+(9, 'Raymond', 1500),
+(10, 'Charles', 1000),
+(11, 'HXR', 900),
+(12, 'PINK', 1200);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `city`, `address`) VALUES
+(1, 'minh', 'minhnguyen1412h2@gmail.com', '14e1b600b1fd579f47433b88e8d85291', '1312321', 'HCM', 'Q7'),
+(2, 'admin', 'admin@gmail.com', '14e1b600b1fd579f47433b88e8d85291', '5676657', 'HCM', 'Q7');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_items`
+--
+
+CREATE TABLE `users_items` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `status` enum('Added to cart','Confirmed') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_items`
+--
+
+INSERT INTO `users_items` (`id`, `user_id`, `item_id`, `status`) VALUES
+(7, 3, 3, 'Added to cart'),
+(8, 3, 4, 'Added to cart'),
+(9, 3, 5, 'Added to cart'),
+(10, 3, 11, 'Added to cart'),
+(11, 1, 9, 'Added to cart'),
+(12, 1, 2, 'Added to cart'),
+(13, 1, 8, 'Added to cart'),
+(14, 4, 2, 'Confirmed'),
+(18, 5, 11, 'Added to cart'),
+(20, 5, 5, 'Added to cart');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_items`
+--
+ALTER TABLE `users_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`,`item_id`),
+  ADD KEY `item_id` (`item_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users_items`
+--
+ALTER TABLE `users_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_items`
+--
+ALTER TABLE `users_items`
+  ADD CONSTRAINT `users_items_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `users_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
+COMMIT;
+
+
